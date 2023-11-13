8452eeefdf7b45d2d1362c04cd1db560429b4ad5
ANDERSON CEZAR FIOS <andersonfios@gmail.com>
2023-11-13 17:00:09 -0300

    Atualização de código

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+U
+%src/test/java/com/meuapp/AppTest.java,d\f\dfd8f81558496a30e0e5807f2054f5e0ea14e15e
+X
+(src/main/java/com/meuapp/OakService.java,5\1\5115a97d99412ff7c67be055755cf9ed59072ae4
+B
+jackrabbit-app.iml,8\a\8aa710918da71ca8231d4f1251aa9389965205d9
--- src/main/java/com/meuapp/OakService.java
+++ src/main/java/com/meuapp/OakService.java
@@ -1,11 +1,13 @@
 package com.meuapp;
 
-import javax.jcr.Repository;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-import javax.jcr.SimpleCredentials;
+import javax.jcr.*;
+import javax.jcr.nodetype.NodeTypeManager;
+import javax.jcr.nodetype.NodeTypeTemplate;
+import javax.jcr.nodetype.PropertyDefinitionTemplate;
 import javax.jcr.security.AccessControlList;
+import javax.jcr.security.Privilege;
 
+import org.apache.jackrabbit.api.security.JackrabbitAccessControlList;
 import org.apache.jackrabbit.commons.jackrabbit.authorization.AccessControlUtils;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.jcr.Jcr;
@@ -24,6 +26,81 @@ public class OakService {
 
             // aqui é possivel implementar códigos para estudo da ferramenta.
 
+
+            NamespaceRegistry namespaceRegistry = session.getWorkspace().getNamespaceRegistry();
+            namespaceRegistry.registerNamespace("my", "http://example.com/my_namespace");
+            session.save();
+
+
+
+            // Criar um template para o novo tipo de nó
+            NodeTypeManager nodeTypeManager = session.getWorkspace().getNodeTypeManager();
+            NodeTypeTemplate nodeTypeTemplate = nodeTypeManager.createNodeTypeTemplate();
+            nodeTypeTemplate.setName("my:book");
+            nodeTypeTemplate.setDeclaredSuperTypeNames(new String[] {"nt:unstructured"});
+
+            // Adicionar propriedades ao tipo de nó
+            ValueFactory valueFactory = session.getValueFactory();
+
+            PropertyDefinitionTemplate titleProp = nodeTypeManager.createPropertyDefinitionTemplate();
+            titleProp.setName("my:title");
+            titleProp.setRequiredType(javax.jcr.PropertyType.STRING);
+            nodeTypeTemplate.getPropertyDefinitionTemplates().add(titleProp);
+
+            PropertyDefinitionTemplate authorProp = nodeTypeManager.createPropertyDefinitionTemplate();
+            authorProp.setName("my:author");
+            authorProp.setRequiredType(javax.jcr.PropertyType.STRING);
+            nodeTypeTemplate.getPropertyDefinitionTemplates().add(authorProp);
+
+            PropertyDefinitionTemplate isbnProp = nodeTypeManager.createPropertyDefinitionTemplate();
+            isbnProp.setName("my:isbn");
+            isbnProp.setRequiredType(javax.jcr.PropertyType.STRING);
+            isbnProp.setMandatory(true);
+            nodeTypeTemplate.getPropertyDefinitionTemplates().add(isbnProp);
+
+            nodeTypeTemplate.setQueryable(true);
+
+            // Registrar o tipo de nó
+            nodeTypeManager.registerNodeType(nodeTypeTemplate, true);
+            session.save();
+
+
+
+
+            nodeTypeTemplate.setName("my:ebook");
+            nodeTypeTemplate.setDeclaredSuperTypeNames(new String[] {"my:book"}); // Herda de my:book
+
+            // Adicionar propriedades ao tipo de nó
+            PropertyDefinitionTemplate fileFormatProp = nodeTypeManager.createPropertyDefinitionTemplate();
+            fileFormatProp.setName("my:fileFormat");
+            fileFormatProp.setRequiredType(javax.jcr.PropertyType.STRING);
+            nodeTypeTemplate.getPropertyDefinitionTemplates().add(fileFormatProp);
+
+            // Registrar o tipo de nó
+            nodeTypeManager.registerNodeType(nodeTypeTemplate, true);
+            session.save();
+
+
+
+
+            Node rootNode = session.getRootNode();
+            Node ebookNode = rootNode.addNode("sampleEbook", "my:ebook");
+            ebookNode.setProperty("my:title", "Sample eBook");
+            ebookNode.setProperty("my:author", "John Doe");
+            ebookNode.setProperty("my:isbn", "1234567890");
+            ebookNode.setProperty("my:fileFormat", "epub");
+            session.save();
+
+
+
+            rootNode = session.getRootNode();
+            Node ebookNodeSearch = rootNode.getNode("sampleEbook");
+
+            String namespaceURI = session.getNamespaceURI("my");
+
+
+
+
             session.logout();
         }
     }
@@ -37,5 +114,30 @@ public class OakService {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void setPermissionsForMyNamespace(String groupName) throws Exception {
+        Node myDocumentsNode = createMyNamespaceDocumentsNode();
+
+        JackrabbitAccessControlList acl = AccessControlUtils.getAccessControlList(getSession(), myDocumentsNode.getPath());
+        if (acl != null) {
+            Privilege[] privileges = {
+                    getSession().getAccessControlManager().privilegeFromName(Privilege.JCR_READ),
+                    getSession().getAccessControlManager().privilegeFromName(Privilege.JCR_WRITE)
+            };
+            acl.addAccessControlEntry(AccessControlUtils.getPrincipal(getSession(), groupName), privileges);
+
+            getSession().getAccessControlManager().setPolicy(myDocumentsNode.getPath(), acl);
+            getSession().save();
+        }
+    }
+
+    public Node createMyNamespaceDocumentsNode() throws Exception {
+        Node rootNode = getSession().getRootNode();
+        if (!rootNode.hasNode("my:documents")) {
+            return rootNode.addNode("my:documents", "nt:folder");
+        } else {
+            return rootNode.getNode("my:documents");
+        }
     }
 }
